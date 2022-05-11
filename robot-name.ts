0c1b73ab67bf0ca7ed9e50481a53fdb29a280f6d
garzoku <garzoku@WGC10316TQCB3.zld.dc.ford.com>
2022-05-11 10:03:37 -0500

    get all tests to pass

--- robot-name.ts
+++ robot-name.ts
@@ -1,4 +1,56 @@
-export class Robot {
+interface IRobot {
+  name: string;
+}
+
+export class Robot implements IRobot {
+  name: string;
+  static usedNames = new Set<string>();
+  static releaseNames() {
+    this.usedNames.clear();
+  }
+  constructor() {
+    this.name = this.uniqueRandomName;
+  }
+
+  resetName() {
+    this.name = this.uniqueRandomName;
+  }
+
+  private get uniqueRandomName() {
+    let newName;
+    do {
+      newName = this.randomName;
+    } while (Robot.usedNames.has(newName));
+    Robot.usedNames.add(newName);
+    return newName;
+  }
+
+  private get randomName() {
+    return `${this.twoRandomLetters}${this.threeRandomDigits}`;
+  }
+
+  private get twoRandomLetters() {
+    const letter1 = this.randomLetter;
+    const letter2 = this.randomLetter;
+    return `${letter1}${letter2}`;
+  }
+
+  private get randomLetter() {
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const number = Math.floor(Math.random() * 26);
+    return letters[number];
+  }
+
+  private get threeRandomDigits() {
+    return `${this.randomNumber}${this.randomNumber}${this.randomNumber}`;
+  }
+
+  private get randomNumber() {
+    const digits = "0123456789";
+    const position = Math.floor(Math.random() * 10);
+    return digits[position];
+  }
+  /*
   readonly alphabet = `ABCDEFGHIJKLMNOPQRSTUVWXYZ`;
   readonly numbers = `0123456789`;
   name: string;
@@ -54,4 +106,5 @@ export class Robot {
     Robot.usedNames = [];
     this.saveName(this.name);
   }
+*/
 }
